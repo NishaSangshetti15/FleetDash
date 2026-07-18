6a5127a43a21c25c973fbe0f40c48f0e241e9627
NishaSangshetti15 <nishasangshetti2004@gmail.com>
2026-07-18 20:53:04 +0530

    Resolve package-lock.json merge conflict

--- Backend/src/routes/vehicleRoutes.js
+++ Backend/src/routes/vehicleRoutes.js
@@ -1,14 +1,22 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {
   createVehicle,
   getVehicles,
+  getVehicle,
+  updateVehicle,
+  deleteVehicle,
 } = require("../controllers/vehicleController");
 
 router.post("/", createVehicle);
 
 router.get("/", getVehicles);
+
+router.get("/:id", getVehicle);
+
+router.put("/:id", updateVehicle);
+
+router.delete("/:id", deleteVehicle);
 
 module.exports = router;
